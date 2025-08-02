f23548bc0df2378b563497386754c4a64e502dad
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-03 00:03:03 +0530

    implement registration logic with input validation

--- backend/src/Routes/authRoutes.js
+++ backend/src/Routes/authRoutes.js
@@ -6,8 +6,16 @@ router.post("/login", async (req, res) => {
     res.send("Login");
 
 });
+
 router.post("/register", async (req, res) => {
-    res.send("Register");  
+    try {
+        const { email, username, password } = req.body;
+
+        if (!email || !username || !password) {
+            return res.status(400).json({ message: "Please fill in all fields" });
+        }
+        
+    } catch (error) {}
 });
 
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -9,9 +9,11 @@ import { connectDB } from './lib/db.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.use("/api/auth",authrouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB();
+  connectDB()
 });
