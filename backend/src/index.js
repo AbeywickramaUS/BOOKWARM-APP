d5af8090f9fa63e805bfcf958bd57472f5bcb3f1
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-02 22:07:56 +0530

    create DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import "dotenv/config";
 
 import authrouter from "./Routes/authRoutes.js";
+import { connectDB } from './lib/db.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,4 +13,5 @@ app.use("/api/auth",authrouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  connectDB();
 });
