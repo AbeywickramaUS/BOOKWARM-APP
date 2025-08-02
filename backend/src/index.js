8a58f5993549e26f041069acf9f372110b08dc34
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-02 21:47:12 +0530

    aaaa

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,15 @@
 //const express = require('express');
 
 import express from 'express';
+import "dotenv/config";
+
+import authrouter from "./Routes/authRoutes.js";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.use("/api/auth",authrouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
