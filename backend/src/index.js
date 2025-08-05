ead0a995113a2fb3909323457d1e2d251d38b161
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-05 22:28:11 +0530

    add book routes to authentication API

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import "dotenv/config";
 
 import authrouter from "./Routes/authRoutes.js";
+import bookrouter from "./Routes/bookRoutes.js";
 import { connectDB } from './lib/db.js';
 
 const app = express();
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.use("/api/auth",authrouter);
+app.use("/api/auth", bookrouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
