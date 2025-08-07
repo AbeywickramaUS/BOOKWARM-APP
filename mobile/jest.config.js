251dfc1a0f9b01d594db6edef8cb2bf898a488de
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-07 13:31:37 +0530

    add jest.config.js for configuring Jest with React Native

---
+++ mobile/jest.config.js
@@ -0,0 +1,5 @@
+export default {
+  preset: 'jest-react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+};
