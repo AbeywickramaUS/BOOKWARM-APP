833e648aaf1a779078b1c7a2ddffd65cc4120163
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-07 14:24:37 +0530

    Remove mobile package.json and tsconfig.json files as part of project restructuring

--- mobile/jest.config.js
+++
@@ -1,5 +0,0 @@
-export default {
-  preset: 'jest-react-native',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
-};
