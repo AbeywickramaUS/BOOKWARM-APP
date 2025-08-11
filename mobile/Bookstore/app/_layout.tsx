fa7de1aed1cfa74e4f4b1cf7d228dd1e3e5ad5fd
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-11 16:50:30 +0530

    refactor: update RootLayout to include stack screens with titles

--- mobile/Bookstore/app/_layout.tsx
+++ mobile/Bookstore/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack screenOptions={ {headerShown: true} }> 
+        <Stack.Screen name="index" options={{ title: "Welcome to BookWarm Mobile!" }} />
+        <Stack.Screen name="books" options={{ title: "Books" }} />
+        <Stack.Screen name="library" options={{ title: "My Library" }} />
+      </Stack>
+  );
 }
