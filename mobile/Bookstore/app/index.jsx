57ca3e96433a955e5841ef7673ae28e6fbb541cd
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-14 18:53:03 +0530

    feat: create index screen with welcome message and authentication links

---
+++ mobile/Bookstore/app/index.jsx
@@ -0,0 +1,36 @@
+import { Link } from "expo-router";
+import{ View, Text, StyleSheet } from "react-native";
+
+export
+    default function Index() {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Welcome to the Bookstore</Text>
+            <Text style={styles.subtitle}>Find your next great read</Text>
+
+            <Link href="(auth)/login" >
+                Login
+            </Link>
+            <Link href="(auth)/signup">
+                Sign Up
+            </Link>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#fff",
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: "bold",
+    },
+    subtitle: {
+        fontSize: 16,
+        color: "#666",
+    },
+});
