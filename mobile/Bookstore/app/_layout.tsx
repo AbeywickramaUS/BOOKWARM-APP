ce0f7095ef55c00a946064ef7d1e84c4f489ff9b
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-20 18:02:18 +0530

    feat: wrap navigation stack in SafeScreen for improved safe area handling

--- mobile/Bookstore/app/_layout.tsx
+++ mobile/Bookstore/app/_layout.tsx
@@ -1,14 +1,21 @@
 import { Stack } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import SafeScreen from "../components/SafeScreen"; // if folder is 'components'
+// OR
+// import SafeScreen from "../component/SafeScreen"; // if folder is 'component'
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
     <SafeAreaProvider>
       <SafeAreaView style={{ flex: 1 }}>
-        <Stack screenOptions={{ headerShown: false }}>
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(auth)" />
-        </Stack>
+        <StatusBar style="dark"/>
+        <SafeScreen>
+          <Stack screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(auth)" />
+          </Stack>
+        </SafeScreen>
       </SafeAreaView>
     </SafeAreaProvider>
   );
