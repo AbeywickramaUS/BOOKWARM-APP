b25da2680dd3193fa5e545180d1887bc8e042907
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-23 19:27:51 +0530

    feat: implement signup screen with username input and keyboard handling

--- mobile/Bookstore/app/(auth)/signup.jsx
+++ mobile/Bookstore/app/(auth)/signup.jsx
@@ -1,9 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, Platform, StyleSheet, TextInput } from 'react-native'
+import { Ionicons } from '@expo/vector-icons';
+import React, { useState } from 'react';
 
-export default function signup() {
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default function Signup() {
+  const [username, setUsername] = useState('');
+
   return (
-    <View>
-      <Text>Sign Up Screen</Text>
-    </View>
+    <KeyboardAvoidingView 
+        style={{ flex: 1 }} 
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
+      <View style={styles.container}>
+        <Text>Sign Up Screen</Text>
+        <Text> BOOKWARM  </Text>
+        <Text>Welcome to the Bookwarm app!</Text>
+
+        <View style={styles.formContainer}>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}> UserName</Text>
+            <View style={styles.inputContainer}>
+              <Ionicons name="person-outline" size={20} color="#666" style={styles.icon} />
+              <TextInput
+                placeholder="Enter your username"
+                placeholderTextColor="#666"
+                style={styles.input}
+                onChangeText={(text) => setUsername(text)}
+                value={username}
+                autoCapitalize="none"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   )
 }
