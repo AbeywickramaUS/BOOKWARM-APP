cef0a56bc7b724705a769708cd477fcd93b9c01d
IT21363702 <99170248+IT21363702@users.noreply.github.com>
2025-08-25 23:02:29 +0530

    feat: enhance TabLayout with screen options and icons for navigation tabs

--- mobile/Bookstore/app/(tabs)/_layou.jsx
+++ mobile/Bookstore/app/(tabs)/_layou.jsx
@@ -1,11 +1,49 @@
 import {Tabs} from 'expo-router'
+import {Ionicons} from '@expo/vector-icons'
+import colours from '../../constants/Colors'
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 export default function TabLayout() {
+  const insets = useSafeAreaInsets();
   return (
-    <Tabs>
-        <Tabs.Screen name="Home" />
-        <Tabs.Screen name="Create" />
-        <Tabs.Screen name="Profile" />
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: '#007AFF',
+        headerTitleStyle : {
+            color: colours.textPrimary,
+            fontWeight: "600"
+        },
+        headerShadowVisible: false,
+
+        tabBarStyle: {
+            backgroundColor: colours.background,
+            borderTopColor: colours.border,
+            borderTopWidth: 1,
+            paddingTop: 5,
+            paddingBottom: insets.bottom,
+            height: 60 + insets.bottom
+        }
+      }}
+    >
+      <Tabs.Screen name="Home" 
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: (color, size) => (<Ionicons name="home" size={size} color={color}/>)
+        }}
+      />
+      <Tabs.Screen name="Create" 
+        options={{
+          tabBarLabel: "Create",
+          tabBarIcon: ({ color, size }) => (<Ionicons name="add" size={size} color={color}/>)
+        }}
+      />
+      <Tabs.Screen name="Profile" 
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color, size }) => (<Ionicons name="person" size={size} color={color}/>)
+        }}
+      />
     </Tabs>
   )
 }
